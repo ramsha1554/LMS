4d41ab90a2b286649da308f4e13f04ccffe51acf
ramsha132 <work.rm.13.dec@gmail.com>
2025-09-26 23:25:48 +0530

    Merged remote main into local frontend

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Route , Routes} from "react-router-dom"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Home from "./pages/Home"
+
+
+const App = () => {
+  return (
+  <>
+  <Routes>
+<Route path="/" element={<Home />} />
+<Route path="/login" element={<Login />} />
+<Route path="/signup" element={<Signup />} />
+
+
+
+
+  </Routes>
+  
+
+  
+  </>
+  )
+}
+
+export default App
