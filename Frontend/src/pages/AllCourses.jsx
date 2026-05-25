45b99fddc56f8dd62c86098309c379013aec2418
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 11:59:31 +0530

    feat: render dynamic course cards

--- Frontend/src/pages/AllCourses.jsx
+++ Frontend/src/pages/AllCourses.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Nav from '../component/Nav'
 import { useSelector } from 'react-redux'
+import Card from '../component/Card'
 
 function AllCourses() {
 
@@ -11,6 +12,7 @@ function AllCourses() {
       <Nav />
 
       <main className='w-full py-[130px] px-[20px]'>
+
         <h1 className='text-3xl font-bold'>
           All Courses
         </h1>
@@ -18,6 +20,21 @@ function AllCourses() {
         <p className='mt-3 text-gray-600'>
           Total Courses: {courseData?.length}
         </p>
+
+        <div className='flex flex-wrap gap-6 mt-10'>
+          {
+            courseData?.map((course, index) => (
+              <Card
+                key={index}
+                title={course.title}
+                category={course.category}
+                thumbnail={course.thumbnail}
+                price={course.price}
+              />
+            ))
+          }
+        </div>
+
       </main>
     </div>
   )
