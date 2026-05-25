241c336be9432db762cf69d5ff5cd252335db4c7
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 12:05:55 +0530

    feat: create initial course details page

---
+++ Frontend/src/pages/ViewCourse.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { useNavigate } from 'react-router-dom';
+
+function ViewCourse() {
+
+    const navigate = useNavigate()
+
+  return (
+    <div className='min-h-screen bg-gray-50 p-6'>
+
+        <div className='max-w-5xl mx-auto bg-white rounded-xl shadow-md p-6'>
+
+            <FaArrowLeftLong
+             className='text-black w-[22px] h-[22px] cursor-pointer mb-5'
+             onClick={()=>navigate("/")}
+            />
+
+            <h1 className='text-3xl font-bold'>
+                Course Details
+            </h1>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ViewCourse
