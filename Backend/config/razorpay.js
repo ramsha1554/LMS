883750f72c3550a6eeaaeedc7528cf89869f1bb2
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 13:13:02 +0530

    feat: configure Razorpay payment instance

---
+++ Backend/config/razorpay.js
@@ -0,0 +1,14 @@
+import Razorpay from 'razorpay'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const RazorPayInstance = new Razorpay({
+
+    key_id: process.env.RAZORPAY_KEY_ID,
+
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+
+})
+
+export default RazorPayInstance
