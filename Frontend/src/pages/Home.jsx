f2a33f50817753fa8b338a114a30c8a500b2d44c
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 16:23:26 +0530

    feat: implement LMS homepage hero section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,15 +1,73 @@
 import Nav from "../component/Nav"
+import home from "../assets/home1.jpg"
+import { SiViaplay } from "react-icons/si";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div className="w[100%] overflow-hidden">
-      <div className="w[100%]  h-[140vh] relative"> <Nav/></div>
-      
 
+    <div className='w-full overflow-hidden'>
 
+      {/* Hero Section */}
+
+      <div className='w-full lg:h-[140vh] h-[70vh] relative'>
+
+        <Nav />
+
+        <img
+         src={home}
+         className='object-cover md:object-fill w-full lg:h-full h-[50vh]'
+         alt=""
+        />
+
+        {/* Hero Text */}
+
+        <span className='lg:text-[70px] absolute md:text-[40px] lg:top-[10%] top-[15%] w-full flex items-center justify-center text-white font-bold text-[20px]'>
+
+          Grow Your Skills to Advance
+
+        </span>
+
+        <span className='lg:text-[70px] text-[20px] md:text-[40px] absolute lg:top-[18%] top-[20%] w-full flex items-center justify-center text-white font-bold'>
+
+          Your Career Path
+
+        </span>
+
+        {/* Buttons */}
+
+        <div className='absolute lg:top-[30%] top-[75%] md:top-[80%] w-full flex items-center justify-center gap-3 flex-wrap'>
+
+          <button
+           className='px-[20px] py-[10px] border-2 lg:border-white border-black lg:text-white text-black rounded-[10px] text-[18px] font-light flex gap-2 cursor-pointer'
+           onClick={()=>navigate("/allcourses")}
+          >
+
+            View All Courses
+
+            <SiViaplay className='w-[30px] h-[30px] lg:fill-white fill-black' />
+
+          </button>
+
+          <button
+           className='px-[20px] py-[10px] lg:bg-white bg-black lg:text-black text-white rounded-[10px] text-[18px] font-light cursor-pointer'
+          >
+
+            Search Courses
+
+          </button>
+
+        </div>
+
+      </div>
 
     </div>
+
   )
+
 }
 
 export default Home
