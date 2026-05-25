cf292bdf51b2680640108ed5ce39e4b7e9d4c608
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 11:35:23 +0530

    refactor: remove duplicated nested frontend and backend projects

--- Frontend/Backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const isAuth = require('../middleware/isAuth.js');
-const getCurrentUser = require('../controllers/userController.js');
-const userRouter = express.Router();
-
-
-userRouter.get('/getcurrentuser', isAuth, getCurrentUser);
-
-module.exports = userRouter;
