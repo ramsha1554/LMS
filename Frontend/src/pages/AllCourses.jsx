42b5314ddc3cd5e5f72937f530f49187b9e4ab39
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 11:48:51 +0530

    feat: create initial all courses page

---
+++ Frontend/src/pages/AllCourses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../component/Nav'
+
+function AllCourses() {
+  return (
+    <div>
+      <Nav />
+      <h1>All Courses</h1>
+    </div>
+  )
+}
+
+export default AllCourses
