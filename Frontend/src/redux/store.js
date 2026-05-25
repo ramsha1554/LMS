d8b188289a90ebb64f189ee2151f2905dfc75b4e
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 17:15:28 +0530

    fix: register course reducer in Redux store

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,10 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userSlice from './userSlice';
+import { configureStore } from "@reduxjs/toolkit"
 
+import userReducer from "./userSlice"
+import courseReducer from "./courseSlice"
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-    // Add other reducers here if needed
-  },
-});
+
+    reducer: {
+
+        user: userReducer,
+
+        course: courseReducer
+
+    }
+
+})
