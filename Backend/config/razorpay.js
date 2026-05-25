290955c847234e9a2f7dde21ebdc2195ff04e987
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-25 21:00:05 +0530

    refactor: align backend user model and payment config consistency

--- Backend/config/razorpay.js
+++ Backend/config/razorpay.js
@@ -1,14 +1,9 @@
-import Razorpay from 'razorpay'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import Razorpay from "razorpay";
+import "dotenv/config";
 
 const RazorPayInstance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
 
-    key_id: process.env.RAZORPAY_KEY_ID,
-
-    key_secret: process.env.RAZORPAY_KEY_SECRET
-
-})
-
-export default RazorPayInstance
+export default RazorPayInstance;
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,39 +1,36 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
 
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
 const userSchema = mongoose.Schema(
-
-    {
-
-
-name:{ type: String, required: true},
-description:{ type: String },
-email:{ type: String, required: true, unique: true , validate:[validator.isEmail, 'Please provide a valid email']},
-password:{ type: String, required: true},
-role:{ type: String, enum: ['student', 'educator'], default: 'student' },
-photo:{ type: String , default: 'https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg' },
-enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
-
-  
-
-   
-
-    resetotp:{ type: String },
-    otpExpiry:{ type: Date },   
-    isOtpVerified:{ type: Boolean, default: false }
-,
+  {
+    name: { type: String, required: true },
+    description: { type: String },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "Please provide a valid email"],
     },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["student", "educator"],
+      default: "student",
+    },
+    photo: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+    resetotp: { type: String },
+    otpExpiry: { type: Date },
+    isOtpVerified: { type: Boolean, default: false },
+  },
+  { timestamps: true },
+);
 
-    { timestamps: true }
-)
+const User = mongoose.model("User", userSchema);
 
-
-
-
-
-
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+export default User;
