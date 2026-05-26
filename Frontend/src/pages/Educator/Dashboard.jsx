ee0e58aeec3ddad68cbc1a32b7b2907744a89700
ramsha1554 <work.rm.13.dec@gmail.com>
2026-05-26 13:17:25 +0530

    feat: calculate and display total earnings from enrolled students across all courses

--- Frontend/src/pages/Educator/Dashboard.jsx
+++ Frontend/src/pages/Educator/Dashboard.jsx
@@ -8,6 +8,14 @@ function Dashboard() {
      const { userData } = useSelector(state => state.user)
 
 
+    const { creatorCourseData } = useSelector(state => state.course)
+
+
+ const totalEarning = creatorCourseData?.reduce((sum, course) => {
+        const studentCount = course.enrolledStudents?.length || 0;
+        const courseRevenue = course.price ? course.price * studentCount : 0
+        return sum + courseRevenue;
+    }, 0) || 0
 
 
 
@@ -21,7 +29,7 @@ function Dashboard() {
                     <img src={userData?.photoUrl || userData?.name.slice(0, 1).toUpperCase()}
                         className='w-28 h-28 rounded-full object-cover border-4 border-black shadow-md' alt="Educator" />
                     <div className='text-center md:text-left space-y-1'>
-                        <h1 className='text-2xl font-bold text-gray-800'>Welcome, {userData?.name || "Educator"} 👋</h1>
+                        <h1 className='text-2xl font-bold text-gray-800'>Welcome, {userData?.name || "Educator"} </h1>
                         <p className='text-gray-600 text-sm'>{userData?.description || "Start Creating Courses for Your Students"}</p>
                         <h1 className='px-[10px] text-center py-[10px] border-2 bg-black border-black text-white rounded-[10px] text-[15px] font-light flex items-center justify-center cursor-pointer'
                             onClick={() => navigate("/courses")}>Create Courses</h1>
@@ -29,7 +37,7 @@ function Dashboard() {
 
 
 
-                            
+
                     </div>
                 </div>
 
