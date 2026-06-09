2a89994edeabc2a6f4cf842837415a5369c863f4
ramsha1554 <work.rm.13.dec@gmail.com>
2026-06-09 21:04:51 +0530

    feat: add RequireEducator role guard, protect all educator routes

---
+++ Frontend/src/components/RequireEducator.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const RequireEducator = ({ children }) => {
+  const { userData } = useSelector((state) => state.user);
+
+  if (!userData) return <Navigate to="/login" replace />;
+  if (userData.role !== "educator") return <Navigate to="/" replace />;
+
+  return children;
+};
+
+export default RequireEducator;
+
