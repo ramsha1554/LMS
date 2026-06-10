12b6d6541f6b3e61199e0a66b327178ab4f19731
ramsha1554 <work.rm.13.dec@gmail.com>
2026-06-10 12:04:32 +0530

    feat: add lectureSlice to redux store

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit"
 
 import userReducer from "./userSlice"
 import courseReducer from "./courseSlice"
+import lectureReducer from "./lectureSlice"
 
 export const store = configureStore({
 
@@ -9,7 +10,8 @@ export const store = configureStore({
 
         user: userReducer,
 
-        course: courseReducer
+        course: courseReducer,
+        lecture: lectureReducer
 
     }
 
