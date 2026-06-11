8ab153a06ee90357c7e867cc995b8e155d7bf773
ramsha1554 <work.rm.13.dec@gmail.com>
2026-06-11 12:14:22 +0530

    feat: create reviewSlice and add to Redux store

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,18 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit"
-
-import userReducer from "./userSlice"
-import courseReducer from "./courseSlice"
-import lectureReducer from "./lectureSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import courseReducer from "./courseSlice";
+import lectureReducer from "./lectureSlice";
+import reviewReducer from "./reviewSlice";
 
 export const store = configureStore({
-
-    reducer: {
-
-        user: userReducer,
-
-        course: courseReducer,
-        lecture: lectureReducer
-
-    }
-
-})
+  reducer: {
+    user: userReducer,
+    course: courseReducer,
+    lecture: lectureReducer,
+    review: reviewReducer,
+  },
+});
