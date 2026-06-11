a0cef00a479ceea8c31599daf0e63d999110a110
ramsha1554 <work.rm.13.dec@gmail.com>
2026-06-11 15:29:18 +0530

    fix: initialize courseData as empty array instead of null

--- Frontend/src/redux/courseSlice.js
+++ Frontend/src/redux/courseSlice.js
@@ -3,8 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const courseSlice = createSlice({
     name:"course",
     initialState:{
-        creatorCourseData:null,
-        courseData:null,
+        creatorcourseData:[],
+        courseData:[],
         selectedCourse:null
     },
     reducers:{
