1e17abd377c793bf9f46154cc21f6e09079286c6
ramsha1554 <work.rm.13.dec@gmail.com>
2026-06-12 11:41:23 +0530

    fix: add isLoading state to userSlice

--- Frontend/src/redux/userSlice.js
+++ Frontend/src/redux/userSlice.js
@@ -1,23 +1,22 @@
- import { createSlice } from '@reduxjs/toolkit';
- 
- 
- const userSlice = createSlice({
-  name: 'user',
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
   initialState: {
     userData: null,
-  }, 
-  
-  
-
-    reducers: {
+    isLoading: true,
+  },
+  reducers: {
     setUserData: (state, action) => {
       state.userData = action.payload;
-    }
+      state.isLoading = false;
     },
+    setUserLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+  },
 });
 
-export const { setUserData } = userSlice.actions;
+export const { setUserData, setUserLoading } = userSlice.actions;
 
 export default userSlice.reducer;
-
-
